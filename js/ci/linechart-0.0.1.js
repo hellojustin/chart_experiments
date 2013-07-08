343fe93c5c3dc156a08f5217954b21f47cf4f011
Justin Molineaux <justin.molineaux@gmail.com>
2013-07-08 02:29:48 -0400

    Reorganized creation of Raphael elements to support association of datapoint elements. Legend now working as well.

--- js/ci/linechart-0.0.1.js
+++ js/ci/linechart-0.0.1.js
@@ -3,44 +3,44 @@
 	function LineChart( element, opts ) {
 
     this.element  = element;
+    this.opts     = opts;
     this.canvas   = R( this.element );
     this.width    = this.canvas.width;
     this.height   = this.canvas.height;
     this.labels   = this.extract( 'key',   opts.data );
     this.values   = this.extract( 'value', opts.data );
-    this.gridInfo = this.computeGridInfo( this.values, opts );
-    this.plotInfo = this.computePlotInfo( this.values, this.gridInfo, opts );
-    this.grid     = this.drawGrid( this.gridInfo );
-    this.grid.attr( { 
+    this.gridInfo = this.computeGridInfo();
+    this.plotInfo = this.computePlotInfo();
+
+    this.grid     = this.drawGrid().attr( { 
       'fill'   : opts.gridColor,
       'stroke' : opts.gridColor
     } );
 
+    this.dataPoints = this.drawDataPoints()
+
     this.dataLine = this.canvas.path().attr( {
-      'path'   : this.drawDataLine( this.values, this.plotInfo ),
+      'path'   : this.drawDataLine(),
       'stroke' : opts.dataColor,
       'stroke-width' : 2
     } );
-
-    this.dataPoints = this.drawDataPoints( this.values, this.plotInfo );
-    this.dataPoints.attr( { 
-      'fill'   : opts.dataColor,
-      'stroke' : opts.dataColor
-    } );
   }
 
-  LineChart.prototype.computeGridInfo = function( values, opts ) {
-    var numSpaces   = values.length + 1,
-        origin      = { x : opts.padding.left, 
-                        y : this.height - opts.padding.bottom },
-        topRight    = { x : this.width - opts.padding.right,
-                        y : opts.padding.top }
-        spaceWidth  = ( topRight.x - origin.x ) / numSpaces - opts.gridGap,
+  LineChart.prototype.computeGridInfo = function() {
+    var numSpaces   = this.values.length + 1,
+        origin      = { x : this.opts.padding.left, 
+                        y : this.height - this.opts.padding.bottom - this.opts.legendHeight - this.opts.legendGap },
+        topLeft     = { x : origin.x,
+                        y : this.opts.padding.top },
+        topRight    = { x : this.width - this.opts.padding.right,
+                        y : this.opts.padding.top },
+        spaceWidth  = ( topRight.x - origin.x ) / numSpaces - this.opts.gridGap,
         spaceHeight = ( origin.y - topRight.y );
 
     return {
-      gap         : opts.gridGap,
+      gap         : this.opts.gridGap,
       origin      : origin,
+      topLeft     : topLeft,
       topRight    : topRight,
       numSpaces   : numSpaces,
       spaceWidth  : spaceWidth,
@@ -48,15 +48,17 @@
     }
   }
 
-  LineChart.prototype.computePlotInfo = function( values, gridInfo, opts ) {
-    var max      = this.arrayMax( values ),
+  LineChart.prototype.computePlotInfo = function() {
+    var gridInfo = this.gridInfo,
+        opts     = this.opts,
+        max      = this.arrayMax( this.values ),
         min      = 0,
         range    = max - min,
         origin   = { x : gridInfo.origin.x + (gridInfo.spaceWidth + gridInfo.gap),
                      y : gridInfo.origin.y - opts.plotPadding.bottom },
         topRight = { x : gridInfo.topRight.x - (gridInfo.spaceWidth + gridInfo.gap), 
                      y : gridInfo.topRight.y + opts.plotPadding.top },
-        scale    = { x : ( topRight.x - origin.x ) / (values.length-1),
+        scale    = { x : ( topRight.x - origin.x ) / ( this.values.length-1 ),
                      y : ( origin.y - topRight.y ) / range };
 
     return {
@@ -69,47 +71,67 @@
     }
   }
 
-  LineChart.prototype.drawDataPoints = function( values, plotInfo ) {
-    var pi     = plotInfo,
-        points = this.canvas.set();
-
-    for ( var i = 0; i < values.length; i++ ) {
-      points.push( this.canvas.circle( 
-                   pi.origin.x + pi.scale.x * i, 
-                   pi.origin.y - pi.scale.y * ( values[i] - pi.min ),
-                   4 ) );
+  LineChart.prototype.drawDataPoints = function() {
+    var points = this.canvas.set();
+    for ( var i = 0; i < this.values.length; i++ ) {
+      points.push( this.drawIndividualDataPoint( i, this.values[i], this.labels[i] ) );
     }
-
     return points;
   }
 
-  LineChart.prototype.drawDataLine = function( values, plotInfo ) {
-    var pi = plotInfo,
-        p  = [];
+  LineChart.prototype.drawIndividualDataPoint = function( index, value, label ) {
+    var pi     = this.plotInfo,
+        gi     = this.gridInfo,
+        point  = this.canvas.set(),
+        pointX = pi.origin.x + pi.scale.x * index,
+        pointY = pi.origin.y - pi.scale.y * ( value - pi.min );
+        hotspotWidth = gi.spaceWidth + gi.gap;
 
-    for ( var i = 0; i < values.length; i++ ) {
+    point.push( this.canvas.path().attr( {
+      'path'   : [ 'M', pointX, gi.topLeft.y, 
+                   'L', pointX, gi.origin.y + this.opts.legendGap + this.opts.legendHeight ],
+      'stroke' : '#ffffff',
+      'stroke-width' : this.opts.gridGap
+    } ) )
+
+    point.push( this.canvas.circle( pointX, pointY, 4 ).attr({
+      'stroke' : this.opts.dataColor,
+      'fill'   : this.opts.dataColor
+    }) );
+
+    point.push( this.canvas.text( pointX, gi.origin.y + this.opts.legendGap, label ).attr( {
+      'stroke' : this.opts.dataColor,
+      'fill'   : this.opts.dataColor,
+      'font-size' : this.opts.legendHeight
+    } ) );
+
+    // point.push( this.canvas.rect(
+    //   pointX - hotspotWidth/2, 
+    //   gridInfo.topLeft.y,
+    //   hotspotWidth,
+    //   gridInfo.origin.y - gridInfo.topLeft.y
+    // ) );
+
+    return point;
+  }
+
+  LineChart.prototype.drawDataLine = function() {
+    var pi = this.plotInfo,
+        p  = [];
+    for ( var i = 0; i < this.values.length; i++ ) {
       p = p.concat( [ ( i == 0 ) ? "M" : "L",
                     pi.origin.x + pi.scale.x * i, 
-                    pi.origin.y - pi.scale.y * ( values[i] - pi.min ) ] );
+                    pi.origin.y - pi.scale.y * ( this.values[i] - pi.min ) ] );
     }
-
     return p;
   }
 
-  LineChart.prototype.drawGrid = function( gridInfo ) {
-    var gi        = gridInfo,
-        gridRects = this.canvas.set();
-
-    for ( var i = 0; i < gi.numSpaces; i++ ) {
-      gridRects.push( 
-        this.canvas.rect( gi.origin.x + gi.gap/2 + ( gi.spaceWidth + gi.gap ) * i,
-                          gi.origin.y - gi.spaceHeight,
-                          gi.spaceWidth,
-                          gi.spaceHeight )
-      );
-    }
-
-    return gridRects;
+  LineChart.prototype.drawGrid = function(s) {
+    var gi = this.gridInfo;
+    return this.canvas.rect( gi.topLeft.x,
+                             gi.topLeft.y,
+                             gi.topRight.x - gi.topLeft.x,
+                             gi.origin.y - gi.topLeft.y );
   }
 
   LineChart.prototype.extract = function( part, fromData ) {
